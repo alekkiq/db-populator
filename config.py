e6aea2305d4aabacbddbcab60a7d62cd72df9d19
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2024-09-05 12:26:06 +0300

    Add a config base

---
+++ config.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import os
+
+def config() -> dict:
+    # Load configs from .env file
+    load_dotenv(os.abspath(os.path.join(os.path.dirname(__file__), ".env")))
+
+    return {
+        "connection_params": {
+            "host": os.getenv("DB_HOST"),
+            "port": os.getenv("DB_PORT"),
+            "username": os.getenv("DB_USER"),
+            "password": os.getenv("DB_PASSWORD"),
+            "collation": "utf8mb4_unicode_ci",
+            "autocommit": False
+        },
+        "databases": {
+            # Your databases & tables go here!
+        }
+    }
