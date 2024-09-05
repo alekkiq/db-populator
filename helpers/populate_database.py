70111b6a31d9abf77fadb0c0e0706b2a83c6e324
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2024-09-05 12:57:23 +0300

    Add simple try/except to populate_database.py

--- helpers/populate_database.py
+++ helpers/populate_database.py
@@ -12,24 +12,31 @@ def populate_database(db_name: str, data_file: str, data_types: dict, table_name
     # get the database configuration from a config file
     connection_params = configs["connection_params"]
     
-    # initialize the connection and create the database
-    db = db_connection(connection_params)
-    cursor = db.cursor()
-    create_database(cursor, db_name)
-    
-    # get the necessary things from the csv
-    file_path = get_file_path(data_file)
-    column_names = get_csv_headers(file_path)
-    
-    # create the table with the correct column names / data types
-    create_table(cursor, table_name, column_names, data_types)
-    
-    # continue to inserting the data itself.
-    data_chunksize = configs["connection_params"]["csv_chunksize"]
-    data = get_csv_data(file_path, data_chunksize)
-    insert_data_to_table(cursor, table_name, data, column_names)
-    db.commit()
-    
-    # close the cursor & the database when ready
-    cursor.close()
-    db.close()
+    try:
+        # initialize the connection and create the database
+        db = db_connection(connection_params)
+        cursor = db.cursor()
+        create_database(cursor, db_name)
+        
+        # get the necessary things from the csv
+        file_path = get_file_path(data_file)
+        column_names = get_csv_headers(file_path)
+        
+        # create the table with the correct column names / data types
+        create_table(cursor, table_name, column_names, data_types)
+        
+        # continue to inserting the data itself.
+        data_chunksize = configs["connection_params"]["csv_chunksize"]
+        data = get_csv_data(file_path, data_chunksize)
+        insert_data_to_table(cursor, table_name, data, column_names)
+        db.commit()
+        
+        # close the cursor & the database when ready
+        cursor.close()
+        db.close()
+
+        print("Database setup successfully!")
+        return True
+    except Exception as error:
+        print(f"An error occurred while trying to populate the database.\nError:\n{error}")
+        return False
